fde82b42487bef6f0e16d9a409d4e1a022d9a77e
francolmc <franco.morales@outlook.com>
2023-05-29 09:50:48 -0400

    :sparkles: transparent background

--- lua/plugins/tokyodark.lua
+++ lua/plugins/tokyodark.lua
@@ -5,7 +5,7 @@ function UseColorScheme(color)
     vim.g.tokyodark_enable_italic_keyword = true -- agrega esta línea para habilitar itálicas en keywords
     vim.g.tokyodark_enable_italic_function = true -- agrega esta línea para habilitar itálicas en funciones
     vim.g.tokyodark_enable_bold = true -- agrega esta línea para habilitar negritas en los mismos elementos anteriores
-    vim.g.tokyodark_transparent_background = false
+    vim.g.tokyodark_transparent_background = true
     vim.g.tokyodark_color_gamma = "1.0"
 
     -- Establece las fuentes que tienen soporte para calt y ss01
