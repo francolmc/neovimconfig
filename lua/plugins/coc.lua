2471dc36d2628356adbea631cff374bd4f97fa41
Franco Morales Castro <franco.morales@outlook.com>
2023-06-25 21:34:33 -0400

    feat: ✨ coc servers

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,33 +1,34 @@
 vim.fn['coc#config']('suggest.completionItemKindLabels', {
-  variable = " Variable",
-  constant = " Constant",
-  struct = " Struct",
-  class = " Class",
-  interface = " Interface",
-  text = " Text",
-  enum = " Enum",
-  enumMember = " Enum Member",
-  color = " Color",
-  property = " Property",
-  field = " Field",
-  unit = " Unit",
-  file = " File",
-  value = " Value",
-  event = " Event",
-  folder = " Folder",
-  keyword = " Keyword",
-  snippet = "﬌ Snippet",
-  operator = " Operator",
-  reference = " Reference",
-  typeParameter = " Type Parameter",
-  default = " Default",
+    variable = " Variable",
+    constant = " Constant",
+    struct = " Struct",
+    class = " Class",
+    interface = " Interface",
+    text = " Text",
+    enum = " Enum",
+    enumMember = " Enum Member",
+    color = " Color",
+    property = " Property",
+    field = " Field",
+    unit = " Unit",
+    file = " File",
+    value = " Value",
+    event = " Event",
+    folder = " Folder",
+    keyword = " Keyword",
+    snippet = "﬌ Snippet",
+    operator = " Operator",
+    reference = " Reference",
+    typeParameter = " Type Parameter",
+    default = " Default",
 })
 
 
 -- Configuración de coc.nvim
 vim.cmd('set updatetime=300')
 vim.cmd('autocmd CursorHold * silent call CocActionAsync("highlight")')
-vim.g.coc_global_extensions = {'coc-snippets', 'coc-tsserver', 'coc-json', 'coc-lua', 'coc-html', 'coc-html', 'coc-python', 'coc-css'}
+vim.g.coc_global_extensions = { 'coc-yaml', 'coc-tsserver', 'coc-tailwindcss', 'coc-snippets', 'coc-pyright',
+    'coc-prisma', 'coc-prettier', 'coc-lua', 'coc-json', 'coc-html', 'coc-eslint8', 'coc-emmet', 'coc-docker', 'coc-css' }
 -- vim.g.coc_node_path = '/usr/local/bin/node'
 vim.g.coc_snippet_next = '<tab>'
 vim.g.coc_snippet_prev = '<s-tab>'
